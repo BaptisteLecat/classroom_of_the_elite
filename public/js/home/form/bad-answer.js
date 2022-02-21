4799148e09d8d80f6969335dcc2172e42dadd4a5
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2022-02-21 14:33:45 +0100

    work on deletable input

--- public/js/home/form/bad-answer.js
+++ public/js/home/form/bad-answer.js
@@ -1,8 +1,28 @@
-$('button[name="btn-add-badanswer"]').click(function(event) {
-    event.preventDefault();
-    var length = $(".input-container-wrapper .form-label").length;
-    var label = `<label class="form-label" for="badanswer${length}-content">Mauvaise réponse</label>`;
-    var input = `<input class="form-input" name="badanswer${length}-content" type="text"></input>`;
-    $(".input-container-wrapper").append(label);
-    $(".input-container-wrapper").append(input);
+$(document).ready(function() {
+    $('button[name="btn-add-badanswer"]').click(function(event) {
+        event.preventDefault();
+        var length = $(".input-container-wrapper .form-label").length;
+        var label = `<label class="form-label" for="badanswer-${length}-content">Mauvaise réponse</label>`;
+        var input = `
+        <div class="input-deletable-container">
+            <input class="form-input" name="badanswer-${length}-content" style="flex:3;" type="text">
+            <img class="input-deletable-btn" name="${length}" src="/assets/icons/bin.png" alt="delete">
+        </div>`;
+        var inputbox = `
+        <div class="input-container-wrapper" name="input-box-${length}">
+            ${label}
+            ${input}
+        </div>`;
+        console.log(inputbox);
+        $(".input-container").append(inputbox);
+    });
+
+    $(".input-container").on(
+        "click",
+        ".input-deletable-btn",
+        function() {
+            index = event.target.name;
+            $(`div[name="input-box-${index}"]`).remove();
+        }
+    );
 });
